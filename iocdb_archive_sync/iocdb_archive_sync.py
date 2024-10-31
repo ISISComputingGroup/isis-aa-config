1a99850e24ae8a8ee33a39d2b8c6327cd9233389
Jack Doughty <jackb.doughty@btinternet.com>
2024-10-31 16:58:44 +0000

    Type hinting & ruff

--- iocdb_archive_sync/iocdb_archive_sync.py
+++ iocdb_archive_sync/iocdb_archive_sync.py
@@ -5,24 +5,27 @@ import asyncio
 import ast
 import os
 
-async def archivePV(url: str, session: aiohttp.ClientSession,) -> None:
+
+async def archivePV(url: str, session: aiohttp.ClientSession) -> None:
     response = await session.request("GET", url=url)
-    
+
     if response.status != 200:
         print(f"Could not archive: '{url}'.")
 
-async def main():
 
-    #should probably not hardcode this...
+async def main() -> None:
+    # should probably not hardcode this...
     iocdb = mysql.connector.connect(
         host="localhost",
         user=os.getenv("sql_iocdb_user"),
-        password=os.getenv("sql_iocdb_pass")
+        password=os.getenv("sql_iocdb_pass"),
     )
 
     ioccursor = iocdb.cursor()
 
-    ioccursor.execute("SELECT iocdb.pvinfo.pvname, iocdb.pvinfo.value from iocdb.pvinfo where infoname = 'archive' group by iocdb.pvinfo.pvname")
+    ioccursor.execute(
+        "SELECT iocdb.pvinfo.pvname, iocdb.pvinfo.value from iocdb.pvinfo where infoname = 'archive' group by iocdb.pvinfo.pvname"
+    )
 
     iocdb_pv_names = []
     iocdb_pv_value_types = []
@@ -39,20 +42,19 @@ async def main():
     archive_pv_names = ast.literal_eval(response.text)
 
     async with aiohttp.ClientSession() as session:
-
         tasks = []
 
         for pv in iocdb_pv_names:
-            
-            if pv in archive_pv_names: continue
+            if pv in archive_pv_names:
+                continue
 
             url = f"http://localhost:17665/mgmt/bpl/archivePV?pv={pv}"
-            tasks.append(archivePV(url=url,session=session))
+            tasks.append(archivePV(url=url, session=session))
 
         await asyncio.gather(*tasks, return_exceptions=True)
 
-if __name__ == '__main__':
-    
+
+if __name__ == "__main__":
     asyncio.run(main())
 
 # Next steps would be to make sure that any arguments that have been stored
